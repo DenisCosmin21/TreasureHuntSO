5a86a7d9f801c3ecca5e8ab324218acefb37713b
cosmin.draghici <cosmin.draghici@student.upt.ro>
2025-04-11 14:41:24 +0300

    Added file handling library to remove repetitve error checking on system calls

---
+++ FileLib.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+int openFile(const char *path, const char *mode) { //Mode as characters, r => read, w => write, a => append, etc..
+    int openFlags = O_CREAT;
+
+    if (strcmp(mode, "r") == 0) openFlags |= O_RDONLY;
+    else if (strcmp(mode, "rw") == 0) openFlags |= O_RDWR;
+    else if (strcmp(mode, "ra") == 0) openFlags |= O_RDWR | O_APPEND;
+    else if (strcmp(mode, "w") == 0) openFlags |= O_WRONLY;
+    else if (strcmp(mode, "a") == 0) openFlags |= O_APPEND | O_WRONLY;
+
+    int fd = open(path, openFlags, S_IWUSR | S_IRUSR | S_IROTH);
+
+    if (fd == -1) {
+        perror("Error in oppening file");
+        exit(1);
+    }
+
+    return fd;
+}
+
+void closeFile(const int fd) {
+    if (close(fd) == -1) {
+        perror("Error in closing file");
+        exit(1);
+    }
+}
+
+void writeFile(const int fd, const void *data, const size_t len) {
+    if (write(fd, data, len) < 0) {
+        perror("Error writing to file");
+        exit(1);
+    }
+}
+
+void readFile(const int fd, void *buffer, const size_t len) {
+    if (read(fd, buffer, len) < 0) {
+        perror("Error reading from file");
+        exit(1);
+    }
+}
+
+void makeSymlink(const char *from, const char *to) {
+    if (symlink(from, to) == -1) {
+        perror("Error making symlink");
+        exit(1);
+    }
+}
+
+int existsFile(const char *path) {
+    struct stat st;
+    return stat(path, &st) == 0;
+}
+
+struct stat getFileStat(const char *path) {
+    struct stat st;
+    if (stat(path, &st) == -1) {
+        perror("Error getting stat");
+        exit(1);
+    }
+
+    return st;
+}
+
+struct stat getFdStat(const int fd) {
+    struct stat st;
+    if (fstat(fd, &st) == -1) {
+        perror("Error getting stat");
+        exit(1);
+    }
+
+    return st;
+}
+
+void seekCursor(const int fd, const size_t offset, const int whence) {
+    if (lseek(fd, offset, whence) == -1) {
+        perror("Error seeking cursor");
+        exit(1);
+    }
+}
+
+size_t getFileSize(const int fd) {
+    const struct stat st = getFdStat(fd);
+
+    return st.st_size;
+}
+
+void truncateFile(const int fd, const __off_t length) {
+    if (ftruncate(fd, length) == -1) {
+        perror("Impossible to truncate file");
+        exit(-1);
+    }
+}
+
+
---
+++ FileLib.h
@@ -0,0 +1,29 @@
+//
+// Created by debian on 4/11/25.
+//
+
+#ifndef FILELIB_H
+#define FILELIB_H
+
+int openFile(const char *path, const char *mode);
+
+void closeFile(const int fd);
+
+void readFile(const int fd, void *buffer, size_t len);
+
+void writeFile(const int fd, const void *data, size_t len);
+
+void makeSymlink(const char *from, const char *to);
+
+int existsFile(const char *path);
+
+struct stat getFileStat(const char *path);
+
+struct stat getFdStat(const int fd);
+
+void seekCursor(const int fd, const size_t offset, const int whence);
+
+size_t getFileSize(const int fd);
+
+void truncateFile(const int fd, const __off_t size);
+#endif //FILELIB_H
