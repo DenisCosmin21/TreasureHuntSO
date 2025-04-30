eef9f7cbd58b22750c011668950e04ac40b2954e
cosmin.draghici <cosmin.draghici@student.upt.ro>
2025-05-01 02:12:36 +0300

    Added treasure hub

--- TreasureHub.c
+++ TreasureHub.c
@@ -1,3 +1,20 @@
-//
-// Created by debian on 4/16/25.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "treasureMonitor.h"
+
+
+int main(void) {
+    initTreasureHubProcess();
+    promptForCommand();
+    while (1);
+}
+
+/*char **arguments = splitArguments(operation);
+        printf("%s\n", arguments[0]);
+        if (arguments != NULL) {
+            //Then parse operation else wait for further instructions
+            parseOperation(arguments[1], arguments + 2, strtol(arguments[0], NULL, 10));
+        }*/
